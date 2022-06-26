d387d1e23320a8f71339517caa9aeab892cc2f66
Daniyal <MuhammadDaniyal32>
2022-06-26 13:34:59 +0500

    splash screen done

---
+++ lib/utility/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  Constants._();
+
+  static String get appTitle => "Weather App";
+
+}
---
+++ lib/views/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/utility/app_colors.dart';
+import 'package:weather_app/utility/spacing.dart';
+import 'package:weather_app/utility/utils.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.appbgColor,
+      body: Column(
+        children: [
+          VerticalSpacing(UIExt(context).screenHeight()*0.4),
+          Center(child: Image(image: AssetImage("assets/images/splash_img.png"),)),
+          VerticalSpacing(UIExt(context).screenHeight()*0.29),
+          Text("Powered By Tech Idara",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500)),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 5), () => {
+    Navigator.pushReplacement(
+    context, MaterialPageRoute(builder: (context) => LanddingScreen()));
+    });
+  }
+}
